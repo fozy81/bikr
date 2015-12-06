03c26b4294abab14b683a1be74091fc2fcbadf0c
Tim <fozy81>
2015-12-06 22:05:11 +0000

    test

--- examples/shiny-examples/bikrApp/ui.R
+++ examples/shiny-examples/bikrApp/ui.R
@@ -6,20 +6,8 @@ shinyUI(
   navbarPage( 
   title = 'V3LO-SCAPE - Tracking the quality of bicycle infrastructure in your area',
   tabPanel('Classification',
-          # includeScript("spin.min.js"),
            fluidRow(column(12,uiOutput('pleaseClick'),
-                          leafletOutput("mymap")
-                          #,  
-# tags$script("var spinner = new Spinner().spin();
-#                                                               $( 'div#mymap' ).append(spinner.el);"),
-#                           tags$script('
-#   Shiny.addCustomMessageHandler("map_done",
-#         function(s) {
-#           spinner.stop();
-#           $( "div#mymap" ).remove(spinner);
-#         });')
-#                           
-                          ),
+                          leafletOutput("map")),
 
 tags$div(
   HTML('<a href="https://github.com/fozy81/bikr"><img style="position: absolute; top: 25; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>')),
